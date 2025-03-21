d4f77274219e902044412f32fb3042db8d1a3833
ferasraj <feras.raj@gmail.com>
2025-03-21 21:44:07 +0300

    Rady to use

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 let myLeads = [];
-let oldLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
-let deleteBtn = document.getElementById("del-btn");
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads")); // استرجاع البيانات من localStorage
+const deleteBtn = document.getElementById("del-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads")); // استرجاع البيانات من localStorage
+const tabBtn = document.getElementById("tab-btn");
 
+// الحصول على Local من المتغير
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   rander(myLeads);
+}
+
+//دالة ال API بحيث ياخذ المعلومات من داخل التاب
+//ملاحضه اضافه الاتاحه في ملف JSON
+tabBtn.addEventListener("click", chromee);
+function chromee() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    rander(myLeads);
+  });
 }
 
 // دالة لعرض الروابط
@@ -37,13 +49,26 @@ function saveLead() {
     rander(myLeads);
   }
 }
-// localStorage.clear();
+
 // عند النقر على الزر
 inputBtn.addEventListener("click", saveLead);
 
 // عند الضغط على مفتاح Enter داخل حقل الإدخال
-inputEl.addEventListener("keydown", function (enter) {
-  if (enter.key === "Enter") {
-    saveLead();
+inputEl.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault(); // منع السلوك الافتراضي للمفتاح Enter
+
+    if (inputEl.value.trim() !== "") {
+      // إذا كان هناك نص في حقل الإدخال
+      saveLead();
+    } else {
+      // إذا كان حقل الإدخال فارغًا
+      chromee();
+    }
   }
+});
+
+// عند تحميل الصفحة، يتم التركيز تلقائيًا على حقل الإدخال
+document.addEventListener("DOMContentLoaded", function () {
+  inputEl.focus();
 });
